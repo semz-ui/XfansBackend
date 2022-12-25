fd4df402eabc0bc733591dd3829fab2f0be030e0
semz-ui <michaeltimilehin36@gmail.com>
2022-12-25 14:22:42 +0100

    kj

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post("/", upload.single("postImage"), protect, postImg);
+router.post("/", upload.array("postImage", 5), protect, postImg);
 router.get("/me", protect, getImg);
 router.get("/all", getAllImgs);
 
